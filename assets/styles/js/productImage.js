b8727182613aabdeedaf1ed68f1fc7cde1682bb5
Divya Bhatt <divyabhatt2004@gmail.com>
2026-02-12 17:46:48 +0530

    update

--- assets/styles/js/productImage.js
+++ assets/styles/js/productImage.js
@@ -1,9 +1,20 @@
 import alertify from "alertifyjs";
 
 $(document).ready(function () {
-    $('#trashImage').on('click',function(){
+    $('#trashImage').on('click', function () {
         $('#uploadImage').toggleClass('d-none');
         $('#updateImage').toggleClass('d-none');
+    })
+
+    $("#image-dialog").dialog({
+        autoOpen: false,
+        modal: true,
+        width: 600,
+        title: "Image Preview"
+    });
+
+    $('#viewImage').on('click', function () {
+        $("#image-dialog").dialog("open");
     })
 })
 
